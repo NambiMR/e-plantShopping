931ab6e25d3fa1df145f8091435d0ba6f8203a11
Nambi Muthu Raja <nambimr2004@gmail.com>
2024-07-19 07:56:47 -0400

    calculate cost

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -8,28 +8,37 @@ const CartItem = ({ onContinueShopping }) => {
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
- 
+  const calculateTotalAmount = (items) => {
+        return items.reduce((total, item) => {
+        return total + (item.quantity * item.cost);
+    }, 0);
   };
 
   const handleContinueShopping = (e) => {
-   
+    alert('Functionality to be added for future reference');
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+      dispatch(removeItem(item.name));
+    }
   };
 
   const handleRemove = (item) => {
+     dispatch(removeItem({ id: item.id }));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.quantity * item.cost;
   };
 
   return (
